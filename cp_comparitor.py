8d0c7ccf093c5e2f64b9ba1b44f6e50522b6110c
adamjonz <phasyn8@hotmail.com>
2023-05-31 17:47:48 +0200

    first 'Online-ing' of the base functions, using example Volve

---
+++ cp_comparitor.py
@@ -0,0 +1,190 @@
+import numpy as np
+import pandas as pd
+import scipy
+import ruptures as rpt
+
+
+def piecewiseFunc(bkpt, avg):   #constructor for piecewise function returns np.piecewise(x, bkpts_bool,averages)
+    '''Requires two lists or arrays of equal length to build into a piecewise function constructor
+    '''
+    
+    horz = avg
+    bkpt_bool = []
+    zero = 0
+    zed = np.array(zero) 
+    bkpt = np.append(zed, bkpt)  #zero padding at the head of the trends (means) segments 
+
+    for pts in range(1, len(bkpt)):     # this builds the boolean expressions used as *args in the piecewise builder
+        boool = ((x>=bkpt[pts-1]) & (x<+bkpt[pts]))
+        bkpt_bool.append(boool,)
+    why = np.piecewise(x, bkpt_bool, horz)
+    return why #np.piecewise(x, bkpt_bool, horz)
+
+
+def piecewise_linearRegress(data, pen=10, min_size=300):
+    
+    pen = 10
+    min_size = 300
+    
+    #data = data / data[data.argmax()]
+    
+    def data_bkps(data, pen=pen, min_size=min_size): 
+        '''PELT search method for offline changepoint search analysis.
+        This function takes in a continuous sequence of data and
+        through the minimization of a cost function identifies changepoints:
+        min_size refers to the minimum continuous data length that will be 
+        allowed between change points; pen is the threshold for the search 
+        method, in that higher numbers find fewer changepoints.
+
+        Function returns array of dataset relative changepoints my_bkps'''
+
+
+        model = "l2"
+        algo = rpt.Pelt(model=model, min_size=min_size).fit(data)
+        my_bkps = algo.predict(pen=pen)
+
+        return my_bkps
+    
+    
+    
+    def MSELin (data, Upbound, LwBound):   #Linear regression paramater tool for a dataset
+        dataSeg = data[Upbound:LwBound]
+        slope, intercept = scipy.stats.linregress(dataSeg, y=data[Upbound:LwBound].index.values)
+        return slope, intercept
+    def mean(data, Lwbound, Upbound):
+        #dataSeg = data[Lwbound:Upbound].to_numpy()  # if using pandas DF as input
+        dataSeg = data[Lwbound:Upbound]
+        return np.nanmean(dataSeg)
+    
+    bkps = data_bkps(data, pen=pen, min_size=min_size)
+    bkps = np.array(bkps)#.reshape(1,len(my_bkps))
+    #print(len(bkps), bkps)
+    DataMean = np.zeros(len(bkps)+1)
+    bkps = np.concatenate((np.zeros(1),bkps))
+    print(len(bkps), bkps)
+    #bkps = np.concatenate((bkps,len(data))) 
+    for i in range(1, len(bkps)): 
+        #slp = []
+        #interc = []
+        #slp[i-1] , interc[i-1] = MSELin(data, my_bkps[i], my_bkps[i-1])
+        
+        mu = mean(data, int(bkps[i-1]), int(bkps[i]))
+        DataMean[i] = mu
+        print(i-1,i,mu)
+    #return slp, interc
+    return DataMean, bkps
+
+
+    
+
+def cpCorrelation(cpComparitee, cpComparitor):
+    tee = cpCompConstructor(cpComparitee)
+    tor = cpCompConstructor(cpComparitor)
+    QumeProb = np.array([])
+    k = 1
+    
+    for i in range(1,len(tee)):
+        for j in range(1,len(tor)):
+            #print('iter')
+            #print(tor[j][1],tor[j][2])
+            #print(tee[i][1],tee[i][2])
+            #if tor[j][1] == tee[i][1] and tor[j][2] == tee[i][2]:
+            if Cosine(tor[j][1:], tee[i][1:]) >= 0.9999:
+                print('*****MATCH' + str(Cosine(tor[j][1:], tee[i][1:])))
+                probAdd = np.diff(tor.T[0])
+                #print(tor[i][1],tor[i][2])
+                #print(tee[j][1],tee[j][2])
+                for q in range(j, len(probAdd)):
+                    #CumulativeProb.append(q+np.sum(probAdd[q:]))
+                    #print('added')
+                    #print(tor[j][0]+np.sum(probAdd[q:]))
+                    #QumeProb = np.append(QumeProb, (tor[j][0]+np.sum(probAdd[q:])))
+                    QumeProb = np.append(QumeProb, (tee[i][0]+np.sum(probAdd[q:])))
+                    k = k+1
+            #print('no match')
+    #unique, counts = np.unique(CumulativeProb, return_counts=True)
+    
+    end = QumeProb[QumeProb.argmax()]+1
+    #V = np.array([])
+    V = np.zeros(int(end))
+    unique, counts = np.unique(QumeProb, return_counts=True)
+    #print(QumeProb, unique, V)
+    for r in range(1, len(unique)):
+        #print(r, unique[r], counts[r], end)
+        V[int(unique[r])] = counts[r]/k
+    
+    return V
+
+def cpCompConstructor(cpWell): # Creates a ChangePoint length Rows by 3 columns matrix that will be used to compare sequentially 
+    
+    cpBLNK = cpWell
+    
+    cpCompWellX = np.empty(3*len(cpBLNK[0])).reshape(len(cpBLNK[0]),3)
+    
+    #Setting special case Zero offset (Start of well) condition, to initial values
+    cpCompWellX[0] = np.array((cpBLNK[0,0],cpBLNK[1,0],cpBLNK[1,0]))
+    #for loop follows through with the rest of the ChangePTS
+    for i in range(1,len(cpBLNK[0])):
+
+        cpCompWellX[i] = np.array((cpBLNK[0,i],cpBLNK[1,i-1],cpBLNK[1,i]))
+    return cpCompWellX #Returns matix
+
+
+def combine_vectors_to_matrix_(*vectors):
+    longest = 0
+    
+    for i in range(len(vectors)):
+        if longest < len(vectors[i]):
+            longest = len(vectors[i])
+        
+    num_rows = longest
+    num_cols = len(vectors)
+    print('rows ', longest, 'columns', len(vectors)) 
+    matrix = [[0] * num_cols for _ in range(num_rows)]
+    
+    for i, vector in enumerate(vectors):
+        for j, value in enumerate(vector):
+            #print(j,i)
+            matrix[j][i] = value
+    
+    return matrix
+
+
+#Cosine Similarity *** does not take magnitude into account***
+def Cosine(comparitee_vector, comparitor_vector):
+    dot_product = np.dot(comparitee_vector, comparitor_vector.T)
+    denominator = (np.linalg.norm(comparitee_vector) * np.linalg.norm(comparitor_vector))
+    return dot_product/denominator
+
+    
+# Euclidean Distance
+def Euclidean(comparitee_vector, comparitor_vector):
+    vec1 = comparitee_vector.copy()
+    vec2 = comparitor_vector.copy()
+    if len(vec1)<len(vec2): vec1,vec2 = vec2,vec1
+    #vec2 = np.resize(vec2,(vec1.shape[0],vec1.shape[1]))
+    return np.linalg.norm(vec1-vec2)
+
+
+def Theta(comparitee_vector, comparitor_vector):
+    return np.arccos(Cosine(comparitee_vector, comparitor_vector)) + np.radians(10)
+
+#Triangle Area Similarity – Sector Area Similarity.
+# 1. Angle
+# 2. Euclidean Distance between vectors
+# 3. Magnitude of vectors
+def Triangle(comparitee_vector, comparitor_vector):
+    theta = np.radians(Theta(comparitee_vector, comparitor_vector))
+    return ((np.linalg.norm(comparitee_vector) * np.linalg.norm(comparitor_vector)) * np.sin(theta))/2
+
+#Magnitude Difference
+def Magnitude_Difference(vec1, vec2):
+    return abs((np.linalg.norm(vec1) - np.linalg.norm(vec2)))
+    
+    
+#Sector Area Similarity    
+def Sector(comparitee_vector, comparitor_vector):
+    ED = Euclidean(comparitee_vector, comparitor_vector)
+    MD = Magnitude_Difference(comparitee_vector, comparitor_vector)
+    theta = Theta(comparitee_vector, comparitor_vector)
+    return np.pi * (ED + MD)**2 * theta/360
---
+++ well_tools.py
@@ -0,0 +1,125 @@
+'''functions and well log grooming for different_bayes framework'''
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+#class well_tools(object):
+
+
+def fill_out_data_ends(data):
+
+    '''replaces NaN values at the ends of an incomplete dataset with first or last value encountered'''
+
+    lmin = np.min(np.where(np.isnan(data)==False))
+    lmax = np.max(np.where(np.isnan(data)==False))
+    if lmin > 0:
+        data[:lmin] = data[lmin]
+    if lmax < len(data):
+        data[lmax:] = data[lmax]
+    return data
+
+
+
+def remove_nan(data, replace_with='glob_avg'):
+
+    '''Replace NaN values with average of all input data'''
+    
+    if replace_with == 'glob_avg':
+  
+        data_with_removed_NaN = data[~np.isnan(data)]
+        mu = np.mean(data_with_removed_NaN)
+        data[np.isnan(data)] = mu
+    
+    #   elif replace_with = 'local_avg'
+    #       avg = df.rolling(periods=2, window=30)
+
+    return data
+
+def df_rolling_avg(data, datalabel, window=30, periods=3):
+
+    _window = window
+    _periods = periods        
+
+    '''takes a data segment and constructs a pandas dataframe that is a rolling average
+    of the input'''
+
+    string = 'Roll_Avg_'+ str(datalabel)
+    
+    #df = pd.DataFrame(data, columns=[datalabel])
+    df= pd.DataFrame()
+    df[string] = data[str(datalabel)].rolling(_window, min_periods=_periods).mean() 
+
+    return df
+
+def compute_all_rolling_avg(data, window, periods):
+
+    '''compute rolling average, takes in a pandas dataframe, an integer period and number of
+    windows. Appends rolling average for all column datas and outputs df, retaining originals'''
+    
+    _window = window
+    _periods = periods 
+    string = ""
+    df = pd.DataFrame(data)
+ 
+    _datalabels = df.columns.unique()
+    for datalabel in _datalabels:
+        string = 'Roll_Avg_'+ str(datalabel)
+    
+        #df = pd.DataFrame(df, columns=[datalabel])
+        
+        df[string] = df[datalabel].rolling(_window, min_periods=_periods).mean()
+        
+    return df
+
+
+def label_generator(df_well, df_tops, column_depth, label_name): #From Yohanes Nuwara
+    """
+    Generate Formation (or other) Labels to Well Dataframe
+    (useful for EDA and machine learning purpose)
+
+    Input:
+
+    df_well is your well dataframe (that originally doesn't have the intended label)
+    df_tops is your label dataframe (this dataframe should ONLY have 2 columns)
+        1st column is the label name (e.g. formation top names)
+        2nd column is the depth of each label name
+
+    column_depth is the name of depth column on your df_well dataframe
+    label_name is the name of label that you want to produce (e.g. FM. LABEL)
+
+    Output:
+
+    df_well is your dataframe that now has the labels (e.g. FM. LABEL)
+    """
+
+
+    # generate list of formation depths and top names
+    fm_tops = df_tops.iloc[:,0]  
+    fm_depths = df_tops.iloc[:,1] 
+
+    # create FM. LABEL column to well dataframe
+    # initiate with NaNs
+    df_well[label_name] = np.full(len(df_well), np.nan)  
+
+    indexes = []
+    topnames = []
+    for j in range(len(fm_depths)):
+        # search index at which the DEPTH in the well df equals to OR
+        # larger than the DEPTH of each pick in the pick df
+        if (df_well[column_depth].iloc[-1] > fm_depths[j]):
+            index = df_well.index[(df_well[column_depth] >= fm_depths[j])][0]
+            top = fm_tops[j]
+            indexes.append(index)
+            topnames.append(top)
+
+        # replace the NaN in the LABEL column of well df
+        # at the assigned TOP NAME indexes
+            df_well[label_name].loc[indexes] = topnames
+
+        # Finally, using pandas "ffill" to fill all the rows 
+        # with the TOP NAMES
+            df_well = df_well.fillna(method='ffill')  
+
+    return df_well
